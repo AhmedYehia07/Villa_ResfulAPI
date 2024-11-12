9c572704df94292173961ddf26ce7749b390d159
Ahmed Yehia <150455756+AhmedYehia07@users.noreply.github.com>
2024-11-12 21:28:06 +0200

    Add Base Servise to MVC project

---
+++ Villa_Web/Services/BaseService.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using Villa_Utility;
+using Villa_Web.Models;
+using Villa_Web.Services.IServices;
+
+namespace Villa_Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public APIResponse responseModel { get; set; }
+        public IHttpClientFactory httpClientFactory { get; set; }
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            this.responseModel = new APIResponse();
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> SendAsync<T>(APIRequest apiRequest)
+        {
+            try
+            {
+                var Client = httpClientFactory.CreateClient("VillaAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.URL);
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),Encoding.UTF8, "application/json");
+                }
+                switch(apiRequest.ApiType)
+                {
+                    case SD.ApiType.Post:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.Put:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.Delete:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                HttpResponseMessage apiResponse = null;
+                apiResponse = await Client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                return APIResponse;
+            }
+            catch(Exception ex) 
+            {
+                var dto = new APIResponse
+                {
+                    ErrorMessages = new List<string>() { Convert.ToString(ex) },
+                    IsSuccess = false
+                };
+                var res = JsonConvert.SerializeObject(dto);
+                var APIResponse = JsonConvert.DeserializeObject<T>(res);
+                return APIResponse;
+            }
+
+        }
+    }
+}

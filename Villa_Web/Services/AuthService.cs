c2071eef476699dfc602706cf7cb43ee2ce78985
Ahmed Yehia <150455756+AhmedYehia07@users.noreply.github.com>
2024-12-09 14:09:33 +0200

    Add Authorization service to MVC

---
+++ Villa_Web/Services/AuthService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Villa_Utility;
+using Villa_Web.Models;
+using Villa_Web.Models.DTO;
+using Villa_Web.Services.IServices;
+
+namespace Villa_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private string? VillaURL;
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+            VillaURL = configuration.GetValue<string>("ServiceUrls:VillaApi");
+        }
+        ///api/Users/Login
+        public Task<T> LoginAsync<T>(LoginRequestDto loginRequest)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.Post,
+                Data = loginRequest,
+                URL = VillaURL + "api/Users/Login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterRequestDto registerRequest)
+        {
+            return SendAsync<T>(new APIRequest
+            {
+                ApiType = SD.ApiType.Post,
+                Data = registerRequest,
+                URL = VillaURL + "api/Users/Register"
+            });
+        }
+    }
+}
---
+++ Villa_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Villa_Web.Models.DTO;
+
+namespace Villa_Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDto loginRequest);
+        Task<T> RegisterAsync<T>(RegisterRequestDto registerRequest);
+    }
+}
